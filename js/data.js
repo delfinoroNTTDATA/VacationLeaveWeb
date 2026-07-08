d89b2e06ed9fa17e114ea8967eea4d2edaca5aa3
robertodelfino <roberto.delfino@emeal.nttdata.com>
2026-07-08 11:33:58 +0200

    Add initial Firestore data loading and saving setup in data.js

---
+++ js/data.js
@@ -0,0 +1,6 @@
+// ============================================
+// data.js — Firestore Data Loading and Saving
+// ============================================
+
+import { db, evRef, ripRef, session} from "./firebase-config";
+import { S } from  './state'
